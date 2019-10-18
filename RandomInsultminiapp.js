b5f1348ec19130a988db781c6716cffd502f681c
Yaswanth <yskchowdary4@gmail.com>
2019-10-18 04:01:11 -0400

    new

---
+++ RandomInsultminiapp.js
@@ -0,0 +1,7 @@
+var Randomwords=["leg","eyes","Nose","pain"];
+var RandomAdjects=["is","that","there","where","must"];
+var Randomthings=["noise","major","stop","beautiful"];
+var word=Randomwords[Math.floor(Math.random()*Randomwords.length )];
+var word1=RandomAdjects[Math.floor(Math.random()*RandomAdjects.length )];
+var word2=Randomthings[Math.floor(Math.random()*Randomthings.length )];
+console.log(word+" "+word1+" "+word2);
